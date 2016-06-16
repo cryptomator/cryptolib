a6322588a895d001ae028ca0366fddcd293ea127
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2016-06-16 03:03:23 +0200

    yet another Java 8 api use...

--- src/test/java/org/cryptomator/cryptolib/SecureRandomMock.java
+++ src/test/java/org/cryptomator/cryptolib/SecureRandomMock.java
@@ -11,42 +11,44 @@ package org.cryptomator.cryptolib;
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.function.Consumer;
 
-// TODO make class package-private
-public class SecureRandomMock extends SecureRandom {
+class SecureRandomMock extends SecureRandom {
 
-	private static final Consumer<byte[]> NULL_FILLER = new Consumer<byte[]>() {
+	private static final ByteFiller NULL_FILLER = new ByteFiller() {
 
 		@Override
-		public void accept(byte[] bytes) {
+		public void fill(byte[] bytes) {
 			Arrays.fill(bytes, (byte) 0x00);
 		}
 
 	};
 	public static final SecureRandomMock NULL_RANDOM = new SecureRandomMock(NULL_FILLER);
-	private static final Consumer<byte[]> PRNG_FILLER = new Consumer<byte[]>() {
+	private static final ByteFiller PRNG_FILLER = new ByteFiller() {
 
 		private final Random random = new Random();
 
 		@Override
-		public void accept(byte[] bytes) {
+		public void fill(byte[] bytes) {
 			random.nextBytes(bytes);
 		}
 
 	};
 	public static final SecureRandomMock PRNG_RANDOM = new SecureRandomMock(PRNG_FILLER);
 
-	private final Consumer<byte[]> byteFiller;
+	private final ByteFiller byteFiller;
 
-	public SecureRandomMock(Consumer<byte[]> byteFiller) {
+	public SecureRandomMock(ByteFiller byteFiller) {
 		this.byteFiller = byteFiller;
 	}
 
 	@Override
 	public void nextBytes(byte[] bytes) {
-		byteFiller.accept(bytes);
+		byteFiller.fill(bytes);
 
+	}
+
+	private static interface ByteFiller {
+		void fill(byte[] bytes);
 	}
 
 }
