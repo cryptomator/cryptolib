f1572d05020fcf87dbc1978cc8cebcff172cdfd9
Markus Kreusch <mail@markuskreusch.de>
2016-09-08 16:41:25 +0200

    Made CryptoLibComponent public

--- src/main/java/org/cryptomator/cryptolib/CryptoLibComponent.java
+++ src/main/java/org/cryptomator/cryptolib/CryptoLibComponent.java
@@ -15,7 +15,7 @@ import org.cryptomator.cryptolib.api.CryptorProvider;
 import dagger.Component;
 
 @Component(modules = {CryptoLibModule.class})
-interface CryptoLibComponent {
+public interface CryptoLibComponent {
 
 	@CryptoLibVersion(Version.ONE)
 	CryptorProvider version1();
