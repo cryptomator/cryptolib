5d0da6bad0940b5b455464f2480f5d8b6a27d593
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2021-08-18 15:30:10 +0200

    cleanup

--- src/main/java/org/cryptomator/cryptolib/common/X509CertBuilder.java
+++ src/main/java/org/cryptomator/cryptolib/common/X509CertBuilder.java
@@ -3,6 +3,7 @@ package org.cryptomator.cryptolib.common;
 import com.google.common.base.Preconditions;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
@@ -121,22 +122,23 @@ public class X509CertBuilder {
 	 */
 	public X509Certificate build() throws CertificateException, IllegalStateException {
 		validate();
-		try {
-			X509v3CertificateBuilder certificateBuilder = new JcaX509v3CertificateBuilder( //
-					issuer, //
-					randomSerialNo(), //
-					notBefore, //
-					notAfter, //
-					subject, //
-					keyPair.getPublic());
-			certificateBuilder.addExtension(ASN1_SUBJECT_KEY_ID, false, getX509ExtensionUtils().createSubjectKeyIdentifier(keyPair.getPublic()));
-			X509CertificateHolder cert = certificateBuilder.build(signer);
-			try (InputStream in = new ByteArrayInputStream(cert.getEncoded())) {
-				return (X509Certificate) getCertFactory().generateCertificate(in);
-			}
+		try (InputStream in = new ByteArrayInputStream(buildCertHolder().getEncoded())) {
+			return (X509Certificate) getCertFactory().generateCertificate(in);
 		} catch (IOException e) {
 			throw new CertificateException(e);
 		}
+	}
+
+	private X509CertificateHolder buildCertHolder() throws CertIOException {
+		X509v3CertificateBuilder certificateBuilder = new JcaX509v3CertificateBuilder( //
+				issuer, //
+				randomSerialNo(), //
+				notBefore, //
+				notAfter, //
+				subject, //
+				keyPair.getPublic());
+		certificateBuilder.addExtension(ASN1_SUBJECT_KEY_ID, false, getX509ExtensionUtils().createSubjectKeyIdentifier(keyPair.getPublic()));
+		return certificateBuilder.build(signer);
 	}
 
 	private static BigInteger randomSerialNo() {
