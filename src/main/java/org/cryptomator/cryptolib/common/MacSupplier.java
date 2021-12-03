bf462146d6a770446b493e310a22c0e943ae5e2b
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2021-12-03 14:44:23 +0100

    use object pool in MacSupplier

--- src/main/java/org/cryptomator/cryptolib/common/MacSupplier.java
+++ src/main/java/org/cryptomator/cryptolib/common/MacSupplier.java
@@ -8,42 +8,71 @@
  *******************************************************************************/
 package org.cryptomator.cryptolib.common;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
+import javax.crypto.Cipher;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public final class MacSupplier {
 
 	public static final MacSupplier HMAC_SHA256 = new MacSupplier("HmacSHA256");
 
 	private final String macAlgorithm;
-	private final ThreadLocal<Mac> threadLocal;
+	private final ObjectPool<Mac> macPool;
 
 	public MacSupplier(String macAlgorithm) {
 		this.macAlgorithm = macAlgorithm;
-		this.threadLocal = new Provider();
-	}
-
-	private class Provider extends ThreadLocal<Mac> {
-		@Override
-		protected Mac initialValue() {
-			try {
-				return Mac.getInstance(macAlgorithm);
-			} catch (NoSuchAlgorithmException e) {
-				throw new IllegalArgumentException("Invalid MAC algorithm.", e);
-			}
+		this.macPool = new ObjectPool<>(this::createMac);
+		try (ObjectPool<Mac>.Lease lease = macPool.get()) {
+			lease.get(); // eagerly initialize to provoke exceptions
 		}
 	}
 
-	public Mac withKey(SecretKey key) {
+	private Mac createMac() {
 		try {
-			final Mac mac = threadLocal.get();
+			return Mac.getInstance(macAlgorithm);
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException("Invalid MAC algorithm.", e);
+		}
+	}
+
+	public ReusableMac keyed(SecretKey key) {
+		ObjectPool<Mac>.Lease lease = macPool.get();
+		init(lease.get(), key);
+		return new ReusableMac(lease);
+	}
+
+	@Deprecated
+	public Mac withKey(SecretKey key) {
+		final Mac mac = createMac();
+		init(mac, key);
+		return mac;
+	}
+
+	private void init(Mac mac, SecretKey key) {
+		try {
 			mac.init(key);
-			return mac;
 		} catch (InvalidKeyException e) {
 			throw new IllegalArgumentException("Invalid key.", e);
+		}
+	}
+
+	public static class ReusableMac implements AutoCloseable {
+
+		private final ObjectPool<Mac>.Lease lease;
+
+		private ReusableMac(ObjectPool<Mac>.Lease lease) {
+			this.lease = lease;
+		}
+
+		public Mac get() {
+			return lease.get();
+		}
+
+		@Override
+		public void close() {
+			lease.close();
 		}
 	}
 
