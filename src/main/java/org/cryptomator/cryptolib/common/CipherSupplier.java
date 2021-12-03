05554f6b0a5326a5f10479b49e24b6ca55386dee
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2021-12-03 14:28:29 +0100

    use object pool in CipherSupplier

--- src/main/java/org/cryptomator/cryptolib/common/CipherSupplier.java
+++ src/main/java/org/cryptomator/cryptolib/common/CipherSupplier.java
@@ -15,7 +15,6 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.AlgorithmParameterSpec;
-import java.util.function.Function;
 
 public final class CipherSupplier {
 
@@ -24,51 +23,101 @@ public final class CipherSupplier {
 	public static final CipherSupplier RFC3394_KEYWRAP = new CipherSupplier("AESWrap");
 
 	private final String cipherAlgorithm;
-	private final ThreadLocal<Cipher> threadLocal;
+	private final ObjectPool<Cipher> cipherPool;
 
 	public CipherSupplier(String cipherAlgorithm) {
 		this.cipherAlgorithm = cipherAlgorithm;
-		this.threadLocal = new Provider();
-		this.threadLocal.get(); // eagerly initialize to provoke exceptions
-	}
-
-	private class Provider extends ThreadLocal<Cipher> {
-		@Override
-		protected Cipher initialValue() {
-			try {
-				return Cipher.getInstance(cipherAlgorithm);
-			} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-				throw new IllegalArgumentException("Invalid cipher algorithm or padding.", e);
-			}
+		this.cipherPool = new ObjectPool<>(this::createCipher);
+		try (ObjectPool<Cipher>.Lease lease = cipherPool.get()) {
+			lease.get(); // eagerly initialize to provoke exceptions
 		}
 	}
 
+	private Cipher createCipher() {
+		try {
+			return Cipher.getInstance(cipherAlgorithm);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+			throw new IllegalArgumentException("Invalid cipher algorithm or padding.", e);
+		}
+	}
+
+	public ReusableCipher encrypt(SecretKey key, AlgorithmParameterSpec params) {
+		ObjectPool<Cipher>.Lease lease = cipherPool.get();
+		initMode(lease.get(), Cipher.ENCRYPT_MODE, key, params);
+		return new ReusableCipher(lease);
+	}
+
+	@Deprecated
 	public Cipher forEncryption(SecretKey key, AlgorithmParameterSpec params) {
-		return forMode(Cipher.ENCRYPT_MODE, key, params);
+		final Cipher cipher = createCipher();
+		initMode(cipher, Cipher.ENCRYPT_MODE, key, params);
+		return cipher;
 	}
 
+	public ReusableCipher decrypt(SecretKey key, AlgorithmParameterSpec params) {
+		ObjectPool<Cipher>.Lease lease = cipherPool.get();
+		initMode(lease.get(), Cipher.DECRYPT_MODE, key, params);
+		return new ReusableCipher(lease);
+	}
+
+	@Deprecated
 	public Cipher forDecryption(SecretKey key, AlgorithmParameterSpec params) {
-		return forMode(Cipher.DECRYPT_MODE, key, params);
+		final Cipher cipher = createCipher();
+		initMode(cipher, Cipher.DECRYPT_MODE, key, params);
+		return cipher;
 	}
 
+	public ReusableCipher wrap(SecretKey kek) {
+		ObjectPool<Cipher>.Lease lease = cipherPool.get();
+		initMode(lease.get(), Cipher.WRAP_MODE, kek, null);
+		return new ReusableCipher(lease);
+	}
+
+	@Deprecated
 	public Cipher forWrapping(SecretKey kek) {
-		return forMode(Cipher.WRAP_MODE, kek, null);
+		final Cipher cipher = createCipher();
+		initMode(cipher, Cipher.WRAP_MODE, kek, null);
+		return cipher;
 	}
 
+	public ReusableCipher unwrap(SecretKey kek) {
+		ObjectPool<Cipher>.Lease lease = cipherPool.get();
+		initMode(lease.get(), Cipher.UNWRAP_MODE, kek, null);
+		return new ReusableCipher(lease);
+	}
+
+	@Deprecated
 	public Cipher forUnwrapping(SecretKey kek) {
-		return forMode(Cipher.UNWRAP_MODE, kek, null);
+		final Cipher cipher = createCipher();
+		initMode(cipher, Cipher.UNWRAP_MODE, kek, null);
+		return cipher;
 	}
 
-	// visible for testing
-	Cipher forMode(int ciphermode, SecretKey key, AlgorithmParameterSpec params) {
-		final Cipher cipher = threadLocal.get();
+	private void initMode(Cipher cipher, int ciphermode, SecretKey key, AlgorithmParameterSpec params) {
 		try {
 			cipher.init(ciphermode, key, params);
-			return cipher;
 		} catch (InvalidKeyException e) {
 			throw new IllegalArgumentException("Invalid key.", e);
 		} catch (InvalidAlgorithmParameterException e) {
 			throw new IllegalArgumentException("Algorithm parameter not appropriate for " + cipher.getAlgorithm() + ".", e);
+		}
+	}
+
+	public static class ReusableCipher implements AutoCloseable {
+
+		private final ObjectPool<Cipher>.Lease lease;
+
+		private ReusableCipher(ObjectPool<Cipher>.Lease lease) {
+			this.lease = lease;
+		}
+
+		public Cipher get() {
+			return lease.get();
+		}
+
+		@Override
+		public void close() {
+			lease.close();
 		}
 	}
 }
